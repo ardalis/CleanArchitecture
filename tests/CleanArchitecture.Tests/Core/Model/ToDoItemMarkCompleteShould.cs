0d6a317ef9445f8054ce3281ad8a4a6ea0187d46
Steve Smith <steve@kentsmiths.com>
2016-10-03 15:28:29 -0400

    wired up xunit and a few simple tests to start

---
+++ tests/CleanArchitecture.Tests/Core/Model/ToDoItemMarkCompleteShould.cs
@@ -0,0 +1,31 @@
+﻿using CleanArchitecture.Core.Model;
+using CleanArchitecture.Core.Events;
+using System.Linq;
+using Xunit;
+
+namespace CleanArchitecture.Tests.Core.Model
+{
+    public class ToDoItemMarkCompleteShould
+    {
+        [Fact]
+        public void SetIsDoneToTrue()
+        {
+            var item = new ToDoItem();
+
+            item.MarkComplete();
+
+            Assert.True(item.IsDone);
+        }
+
+        [Fact]
+        public void RaiseToDoItemCompletedEvent()
+        {
+            var item = new ToDoItem();
+
+            item.MarkComplete();
+
+            Assert.Equal(1, item.Events.Count());
+            Assert.IsType<ToDoItemCompletedEvent>(item.Events.First());
+        }
+    }
+}
