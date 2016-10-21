f62505678691ca6e7cbea56887dd350ff9af4d93
Steve Smith <steve@kentsmiths.com>
2016-10-20 23:58:38 -0400

    Fleshing out ValueObject

---
+++ src/CleanArchitecture.Core/SharedKernel/IgnoreMemberAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CleanArchitecture.Core.SharedKernel
+{
+    // source: https://github.com/jhewlett/ValueObject
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class IgnoreMemberAttribute : Attribute
+    {
+    }
+}
---
+++ src/CleanArchitecture.Web/Api/ToDoItemsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArchitecture.Core.Entities;
+using CleanArchitecture.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Web.Api
+{
+    [Route("api/[controller]")]
+    public class ToDoItemsController : Controller
+    {
+        private readonly IRepository<ToDoItem> _todoRepository;
+
+        public ToDoItemsController(IRepository<ToDoItem> todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
+
+        // GET: api/ToDoItems
+        [HttpGet]
+        public IActionResult List()
+        {
+            var items = _todoRepository.List();
+            return Ok(items);
+        }
+
+        // GET: api/ToDoItems
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var items = _todoRepository.GetById(id);
+            return Ok(items);
+        }
+    }
+}
