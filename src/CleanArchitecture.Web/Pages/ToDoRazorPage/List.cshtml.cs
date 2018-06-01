81b7b72df97d48a70f0799a04c892a0cf62e4a2e
Scott DePouw <scott@ardalis.com>
2018-06-01 10:17:30 -0400

    RazorPage for TODO items now lists TODO items like the TODOController does.

--- src/CleanArchitecture.Web/Pages/ToDoRazorPage/List.cshtml.cs
+++ src/CleanArchitecture.Web/Pages/ToDoRazorPage/List.cshtml.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using CleanArchitecture.Core.Entities;
+using CleanArchitecture.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace CleanArchitecture.Web.Pages.ToDoRazorPage
 {
     public class ListModel : PageModel
     {
+        private readonly IRepository<ToDoItem> _todoRepository;
+
+        public List<ToDoItem> ToDoItems { get; set; }
+
+        public ListModel(IRepository<ToDoItem> todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
+
         public void OnGet()
         {
-
+            ToDoItems = _todoRepository.List();
         }
     }
 }
