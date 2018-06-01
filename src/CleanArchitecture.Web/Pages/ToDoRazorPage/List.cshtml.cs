1266b972dd4cc0ffa22cb39ee8452149489d54c3
Scott DePouw <scott@ardalis.com>
2018-06-01 10:12:28 -0400

    Stubbed out Razor page.

---
+++ src/CleanArchitecture.Web/Pages/ToDoRazorPage/List.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CleanArchitecture.Web.Pages.ToDoRazorPage
+{
+    public class ListModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
