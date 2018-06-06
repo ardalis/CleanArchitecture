6a754a7462ee02cd701bb1da11ce87e09260818c
Scott DePouw <scott@ardalis.com>
2018-06-06 07:22:47 -0400

    ApiToDoItemsControllerListShould now uses CustomWebApplicationFactory.

--- tests/CleanArchitecture.Tests/Integration/Web/ApiToDoItemsControllerListShould.cs
+++ tests/CleanArchitecture.Tests/Integration/Web/ApiToDoItemsControllerListShould.cs
@@ -1,25 +1,28 @@
 ﻿using CleanArchitecture.Core.Entities;
 using CleanArchitecture.Web;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
 using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace CleanArchitecture.Tests.Integration.Web
 {
 
-    public class ApiToDoItemsControllerList : BaseWebTest
+    public class ApiToDoItemsControllerList : IClassFixture<CustomWebApplicationFactory<Startup>>
     {
+        public HttpClient Client { get; }
+
+        public ApiToDoItemsControllerList(CustomWebApplicationFactory<Startup> factory)
+        {
+            Client = factory.CreateClient();
+        }
+
         [Fact]
         public async Task ReturnsTwoItems()
         {
-            var response = await _client.GetAsync("/api/todoitems");
+            var response = await Client.GetAsync("/api/todoitems");
             response.EnsureSuccessStatusCode();
             var stringResponse = await response.Content.ReadAsStringAsync();
             var result = JsonConvert.DeserializeObject<IEnumerable<ToDoItem>>(stringResponse).ToList();
