9392d9a7cd06e35d4a2b9e3856d3094a4d8f187c
Scott DePouw <scott@ardalis.com>
2018-10-03 10:58:22 -0400

    Remove redundant default.

--- src/CleanArchitecture.Core/Entities/ToDoItem.cs
+++ src/CleanArchitecture.Core/Entities/ToDoItem.cs
@@ -5,9 +5,9 @@ namespace CleanArchitecture.Core.Entities
 {
     public class ToDoItem : BaseEntity
     {
-        public string Title { get; set; } 
+        public string Title { get; set; }
         public string Description { get; set; }
-        public bool IsDone { get; private set; } = false;
+        public bool IsDone { get; private set; }
 
         public void MarkComplete()
         {
