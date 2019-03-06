126c59d03d52eebc50ef9ee88c24d167d122e403
Steve Smith <steve@kentsmiths.com>
2019-03-06 16:58:46 -0500

    Add info endpoint and reorg tests.

---
+++ src/CleanArchitecture.Web/Api/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Web.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class BaseApiController : Controller
+    {
+
+    }
+}
---
+++ tests/CleanArchitecture.Tests/Integration/Web/Api/MetaControllerInfo.cs
@@ -0,0 +1,32 @@
+﻿using CleanArchitecture.Core.Entities;
+using CleanArchitecture.Web;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanArchitecture.Tests.Integration.Web
+{
+    public class ApiToDoItemsControllerList : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public ApiToDoItemsControllerList(CustomWebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnsVersionAndLastUpdateDate()
+        {
+            var response = await _client.GetAsync("/info");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("Version", stringResponse);
+            Assert.Contains("Last Updated", stringResponse);
+        }
+    }
+}
