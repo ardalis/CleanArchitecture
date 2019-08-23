877ca99a1a6c0eeafb9b80cc65de6fff7abd0147
Steve Smith <steve@kentsmiths.com>
2019-08-23 12:51:36 -0400

    Adding tests and refactoring handler

---
+++ src/CleanArchitecture.Core/Services/SomeDomainService.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanArchitecture.Core.Services
+{
+    public class SomeDomainService
+    {
+        // TODO: This would handle operations involving multiple aggregates or entities
+    }
+}
---
+++ tests/CleanArchitecture.Tests/Core/Handlers/ItemCompletedEmailNotificationHandlerHandle.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchitecture.Core.Entities;
+using CleanArchitecture.Core.Events;
+using CleanArchitecture.Core.Services;
+using System;
+using Xunit;
+
+namespace CleanArchitecture.Tests.Core.Entities
+{
+    public class ItemCompletedEmailNotificationHandlerHandle
+    {
+        [Fact]
+        public void ThrowsExceptionGivenNullEventArgument()
+        {
+            var handler = new ItemCompletedEmailNotificationHandler();
+
+            Exception ex = Assert.Throws<ArgumentNullException>(() => handler.Handle(null));
+        }
+
+        [Fact]
+        public void DoesNothingGivenEventInstance()
+        {
+            var handler = new ItemCompletedEmailNotificationHandler();
+
+            handler.Handle(new ToDoItemCompletedEvent(new ToDoItem()));
+        }
+    }
+}
