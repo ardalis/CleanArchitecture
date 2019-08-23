9c0a23001449fb953431e6dad74b7726593165cf
Steve Smith <steve@kentsmiths.com>
2019-08-23 12:29:37 -0400

    Getting code coverage working for real this time

---
+++ LoadTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LoadTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
