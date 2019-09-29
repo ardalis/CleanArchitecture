ca9708236049a73326c9c0964416083a6479b6aa
Xeinaemm <piterczech@gmail.com>
2019-09-29 15:11:29 +0200

    .NET Core 3.0, csprojs cleanup, add setup

---
+++ src/CleanArchitecture.Infrastructure/ContainerSetup.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using CleanArchitecture.Core.SharedKernel;
+using CleanArchitecture.Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+
+namespace CleanArchitecture.Infrastructure
+{
+	public static class ContainerSetup
+	{
+		public static IServiceProvider InitializeWeb(Assembly webAssembly, IServiceCollection services) =>
+			new AutofacServiceProvider(BaseAutofacInitialization(setupAction =>
+			{
+				setupAction.Populate(services);
+				setupAction.RegisterAssemblyTypes(webAssembly).AsImplementedInterfaces();
+			}));
+
+		public static IContainer BaseAutofacInitialization(Action<ContainerBuilder> setupAction = null)
+		{
+			var builder = new ContainerBuilder();
+
+			var coreAssembly = Assembly.GetAssembly(typeof(BaseEntity));
+			var infrastructureAssembly = Assembly.GetAssembly(typeof(EfRepository));
+			builder.RegisterAssemblyTypes(coreAssembly, infrastructureAssembly).AsImplementedInterfaces();
+
+			setupAction?.Invoke(builder);
+			return builder.Build();
+		}
+	}
+}
