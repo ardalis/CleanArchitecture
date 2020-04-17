de2354c9ef6e75169ca608d792482c0d766d4362
Steve Smith <steve@kentsmiths.com>
2020-04-17 13:42:23 -0400

    Added registration of any handler implementing IHandle<T>

--- src/CleanArchitecture.Infrastructure/DefaultInfrastructureModule.cs
+++ src/CleanArchitecture.Infrastructure/DefaultInfrastructureModule.cs
@@ -1,16 +1,30 @@
 ﻿using Autofac;
+using CleanArchitecture.Core;
 using CleanArchitecture.Infrastructure.Data;
 using CleanArchitecture.Infrastructure.DomainEvents;
 using CleanArchitecture.SharedKernel.Interfaces;
+using System.Collections.Generic;
+using System.Reflection;
+using Module = Autofac.Module;
 
 namespace CleanArchitecture.Infrastructure
 {
     public class DefaultInfrastructureModule : Module
     {
         private bool _isDevelopment = false;
-        public DefaultInfrastructureModule(bool isDevelopment)
+        private List<Assembly> _assemblies = new List<Assembly>();
+
+        public DefaultInfrastructureModule(bool isDevelopment, Assembly callingAssembly =  null)
         {
             _isDevelopment = isDevelopment;
+            var coreAssembly = Assembly.GetAssembly(typeof(DatabasePopulator));
+            var infrastructureAssembly = Assembly.GetAssembly(typeof(EfRepository));
+            _assemblies.Add(coreAssembly);
+            _assemblies.Add(infrastructureAssembly);
+            if (callingAssembly != null)
+            {
+                _assemblies.Add(callingAssembly);
+            }
         }
 
         protected override void Load(ContainerBuilder builder)
@@ -32,6 +46,9 @@ namespace CleanArchitecture.Infrastructure
                 .InstancePerLifetimeScope();
             builder.RegisterType<EfRepository>().As<IRepository>()
                 .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(_assemblies.ToArray())
+                .AsClosedTypesOf(typeof(IHandle<>));
         }
 
         private void RegisterDevelopmentOnlyDependencies(ContainerBuilder builder)
