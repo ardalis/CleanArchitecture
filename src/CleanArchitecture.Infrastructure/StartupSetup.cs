718954a7f75928f669d41b0549f5b73adaee4a6c
JEN <emekanweke604@gmail.com>
2020-04-26 16:52:31 +0100

    implemented retrieval of connection string from appsettings.json rather than being hardcoded (#111)
    Thanks!

--- src/CleanArchitecture.Infrastructure/StartupSetup.cs
+++ src/CleanArchitecture.Infrastructure/StartupSetup.cs
@@ -6,8 +6,8 @@ namespace CleanArchitecture.Infrastructure
 {
 	public static class StartupSetup
 	{
-		public static void AddDbContext(this IServiceCollection services) =>
+		public static void AddDbContext(this IServiceCollection services, string connectionString) =>
 			services.AddDbContext<AppDbContext>(options =>
-				options.UseSqlite("Data Source=database.sqlite")); // will be created in web project root
+				options.UseSqlite(connectionString)); // will be created in web project root
 	}
 }
