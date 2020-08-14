d69bd7d6589652c28132c70eb4579ad5b1f6b6e6
Steve Smith <steve@kentsmiths.com>
2020-08-14 13:16:53 -0400

    Added Ardalis.Specification Updated Repository to support it Added sample Razor Page to demonstrate it

---
+++ src/CleanArchitecture.Web/Pages/ToDoRazorPage/Incomplete.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using CleanArchitecture.Core.Entities;
+using CleanArchitecture.Core.Specifications;
+using CleanArchitecture.SharedKernel.Interfaces;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Web.Pages.ToDoRazorPage
+{
+    public class IncompleteModel : PageModel
+    {
+        private readonly IRepository _repository;
+
+        public List<ToDoItem> ToDoItems { get; set; }
+
+        public IncompleteModel(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task OnGetAsync()
+        {
+            var spec = new IncompleteItemsSpecification();
+
+            ToDoItems = await _repository.ListAsync(spec);
+        }
+    }
+}
