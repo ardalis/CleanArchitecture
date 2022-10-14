ae959bf5bd9e32949cd76ba1f6dfa7f52cecd5c1
Dan Friedman <dan@thefriedmans.org>
2022-10-14 12:06:00 -0500

    Use ValueObject from https://github.com/ardalis/pluralsight-ddd-fundamentals/blob/main/SharedKernel/src/PluralsightDdd.SharedKernel/ValueObject.cs

--- src/Clean.Architecture.SharedKernel/ValueObject.cs
+++ src/Clean.Architecture.SharedKernel/ValueObject.cs
@@ -1,109 +1,119 @@
-﻿using System.Reflection;
+﻿#nullable disable
 
 namespace Clean.Architecture.SharedKernel;
 
-// source: https://github.com/jhewlett/ValueObject
-public abstract class ValueObject : IEquatable<ValueObject>
+/// <summary>
+/// See: https://enterprisecraftsmanship.com/posts/value-object-better-implementation/
+/// </summary>
+[Serializable]
+public abstract class ValueObject : IComparable, IComparable<ValueObject>
 {
-  private List<PropertyInfo>? properties;
-  private List<FieldInfo>? fields;
+  private int? _cachedHashCode;
 
-  public static bool operator ==(ValueObject? obj1, ValueObject? obj2)
+  protected abstract IEnumerable<object> GetEqualityComponents();
+
+  public override bool Equals(object obj)
   {
-    if (object.Equals(obj1, null))
-    {
-      if (object.Equals(obj2, null))
-      {
-        return true;
-      }
+    if (obj == null)
       return false;
-    }
-    return obj1.Equals(obj2);
-  }
 
-  public static bool operator !=(ValueObject? obj1, ValueObject? obj2)
-  {
-    return !(obj1 == obj2);
-  }
+    if (GetUnproxiedType(this) != GetUnproxiedType(obj))
+      return false;
 
-  public bool Equals(ValueObject? obj)
-  {
-    return Equals(obj as object);
-  }
+    var valueObject = (ValueObject)obj;
 
-  public override bool Equals(object? obj)
-  {
-    if (obj == null || GetType() != obj.GetType()) return false;
-
-    return GetProperties().All(p => PropertiesAreEqual(obj, p))
-        && GetFields().All(f => FieldsAreEqual(obj, f));
-  }
-
-  private bool PropertiesAreEqual(object obj, PropertyInfo p)
-  {
-    return object.Equals(p.GetValue(this, null), p.GetValue(obj, null));
-  }
-
-  private bool FieldsAreEqual(object obj, FieldInfo f)
-  {
-    return object.Equals(f.GetValue(this), f.GetValue(obj));
-  }
-
-  private IEnumerable<PropertyInfo> GetProperties()
-  {
-    if (this.properties == null)
-    {
-      this.properties = GetType()
-          .GetProperties(BindingFlags.Instance | BindingFlags.Public)
-          .Where(p => p.GetCustomAttribute(typeof(IgnoreMemberAttribute)) == null)
-          .ToList();
-
-      // Not available in Core
-      // !Attribute.IsDefined(p, typeof(IgnoreMemberAttribute))).ToList();
-    }
-
-    return this.properties;
-  }
-
-  private IEnumerable<FieldInfo> GetFields()
-  {
-    if (this.fields == null)
-    {
-      this.fields = GetType().GetFields(BindingFlags.Instance | BindingFlags.Public)
-          .Where(p => p.GetCustomAttribute(typeof(IgnoreMemberAttribute)) == null)
-          .ToList();
-    }
-
-    return this.fields;
+    return GetEqualityComponents().SequenceEqual(valueObject.GetEqualityComponents());
   }
 
   public override int GetHashCode()
   {
-    unchecked   //allow overflow
+    if (!_cachedHashCode.HasValue)
     {
-      int hash = 17;
-      foreach (var prop in GetProperties())
-      {
-        var value = prop.GetValue(this, null);
-        hash = HashValue(hash, value);
-      }
-
-      foreach (var field in GetFields())
-      {
-        var value = field.GetValue(this);
-        hash = HashValue(hash, value);
-      }
-
-      return hash;
+      _cachedHashCode = GetEqualityComponents()
+          .Aggregate(1, (current, obj) =>
+          {
+            unchecked
+            {
+              return current * 23 + (obj?.GetHashCode() ?? 0);
+            }
+          });
     }
+
+    return _cachedHashCode.Value;
   }
 
-  private int HashValue(int seed, object? value)
+  public int CompareTo(object obj)
   {
-    var currentHash = value != null
-        ? value.GetHashCode()
-        : 0;
+    var thisType = GetUnproxiedType(this);
+    var otherType = GetUnproxiedType(obj);
 
-    return seed * 23 + currentHash;
+    if (thisType != otherType)
+      return string.Compare(thisType.ToString(), otherType.ToString(), StringComparison.Ordinal);
+
+    var other = (ValueObject)obj;
+
+    var components = GetEqualityComponents().ToArray();
+    var otherComponents = other.GetEqualityComponents().ToArray();
+
+    for (var i = 0; i < components.Length; i++)
+    {
+      var comparison = CompareComponents(components[i], otherComponents[i]);
+      if (comparison != 0)
+        return comparison;
+    }
+
+    return 0;
+  }
+
+  private int CompareComponents(object object1, object object2)
+  {
+    if (object1 is null && object2 is null)
+      return 0;
+
+    if (object1 is null)
+      return -1;
+
+    if (object2 is null)
+      return 1;
+
+    if (object1 is IComparable comparable1 && object2 is IComparable comparable2)
+      return comparable1.CompareTo(comparable2);
+
+    return object1.Equals(object2) ? 0 : -1;
+  }
+
+  public int CompareTo(ValueObject other)
+  {
+    return CompareTo(other as object);
+  }
+
+  public static bool operator ==(ValueObject a, ValueObject b)
+  {
+    if (a is null && b is null)
+      return true;
+
+    if (a is null || b is null)
+      return false;
+
+    return a.Equals(b);
+  }
+
+  public static bool operator !=(ValueObject a, ValueObject b)
+  {
+    return !(a == b);
+  }
+
+  internal static Type GetUnproxiedType(object obj)
+  {
+    const string EFCoreProxyPrefix = "Castle.Proxies.";
+    const string NHibernateProxyPostfix = "Proxy";
+
+    var type = obj.GetType();
+    var typeString = type.ToString();
+
+    if (typeString.Contains(EFCoreProxyPrefix) || typeString.EndsWith(NHibernateProxyPostfix))
+      return type.BaseType;
+
+    return type;
   }
 }
