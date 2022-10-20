a1e317e39a6ead7d3515a4963ef7ee33ed0c8b40
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2022-10-20 17:21:12 -0400

    Add a CreateContributor FastEndpoint

---
+++ src/Clean.Architecture.Web/Endpoints/ContributorEndpoints/Create.CreateContributorRequest.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Clean.Architecture.Web.Endpoints.ContributorEndpoints;
+
+public class CreateContributorRequest
+{
+  public const string Route = "/Contributors";
+
+  [Required]
+  public string? Name { get; set; }
+}
