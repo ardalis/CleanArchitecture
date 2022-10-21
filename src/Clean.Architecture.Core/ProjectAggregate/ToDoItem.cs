88b76378aa531b025aa78ef91051832fa38306ac
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2022-10-21 14:11:52 -0400

    Implement DeleteContributor FastEndpoint

--- src/Clean.Architecture.Core/ProjectAggregate/ToDoItem.cs
+++ src/Clean.Architecture.Core/ProjectAggregate/ToDoItem.cs
@@ -8,7 +8,7 @@ public class ToDoItem : EntityBase
 {
   public string Title { get; set; } = string.Empty;
   public string Description { get; set; } = string.Empty;
-  public int ContributorId { get; private set; }
+  public int? ContributorId { get; private set; }
   public bool IsDone { get; private set; }
 
   public void MarkComplete()
@@ -28,6 +28,11 @@ public class ToDoItem : EntityBase
 
     var contributorAddedToItem = new ContributorAddedToItemEvent(this, contributorId);
     base.RegisterDomainEvent(contributorAddedToItem);
+  }
+
+  public void RemoveContributor()
+  {
+    ContributorId = null;
   }
 
   public override string ToString()
