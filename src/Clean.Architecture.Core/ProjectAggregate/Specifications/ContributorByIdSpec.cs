6b065691edc8f406b301fec0177f9a46d3635e6c
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2022-10-24 12:10:52 -0400

    Implement GetById Contributor FastEndpoint

---
+++ src/Clean.Architecture.Core/ProjectAggregate/Specifications/ContributorByIdSpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+
+namespace Clean.Architecture.Core.ContributorAggregate.Specifications;
+
+public class ContributorByIdSpec : Specification<Contributor>, ISingleResultSpecification
+{
+  public ContributorByIdSpec(int contributorId)
+  {
+    Query
+        .Where(contributor => contributor.Id == contributorId);
+  }
+}
