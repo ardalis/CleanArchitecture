7176ee71163d732f2621689f94b4a08ebe76fd6e
Joseph Wambura <joseph.wambura@pycs.co.ke>
2022-12-22 15:47:14 +0300

    ++ test pages

---
+++ tests/Clean.Architecture.FunctionalTests/Pages/PrivacyPage.cs
@@ -0,0 +1,25 @@
+﻿using Clean.Architecture.Web;
+using Xunit;
+
+namespace Clean.Architecture.FunctionalTests.Pages;
+
+[Collection("Sequential")]
+public class PrivacyPage : IClassFixture<CustomWebApplicationFactory<WebMarker>>
+{
+  private readonly HttpClient _client;
+
+  public PrivacyPage(CustomWebApplicationFactory<WebMarker> factory)
+  {
+    _client = factory.CreateClient();
+  }
+
+  [Fact]
+  public async Task ReturnsViewWithCorrectMessage()
+  {
+    HttpResponseMessage response = await _client.GetAsync("/Privacy");
+    response.EnsureSuccessStatusCode();
+    string stringResponse = await response.Content.ReadAsStringAsync();
+
+    Assert.Contains("Clean.Architecture.Web", stringResponse);
+  }
+}
