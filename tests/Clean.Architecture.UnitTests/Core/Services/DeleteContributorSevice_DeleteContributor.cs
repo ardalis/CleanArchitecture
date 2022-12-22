24fa01718cb9823022dccfca7ef3086138898e93
Joseph Wambura <joseph.wambura@pycs.co.ke>
2022-12-22 17:00:07 +0300

    ++ warnings in tests

--- tests/Clean.Architecture.UnitTests/Core/Services/DeleteContributorSevice_DeleteContributor.cs
+++ tests/Clean.Architecture.UnitTests/Core/Services/DeleteContributorSevice_DeleteContributor.cs
@@ -8,25 +8,24 @@ using MediatR;
 using Moq;
 using Xunit;
 
-namespace Clean.Architecture.UnitTests.Core.Services
+namespace Clean.Architecture.UnitTests.Core.Services;
+
+public class DeleteContributorService_DeleteContributor
 {
-    public class DeleteContributorService_DeleteContributor
-    {
-        private readonly Mock<IRepository<Contributor>> _mockRepo = new Mock<IRepository<Contributor>>();
-        private readonly Mock<IMediator> _mockMediator = new Mock<IMediator>();
-        private readonly DeleteContributorService _service;
+  private readonly Mock<IRepository<Contributor>> _mockRepo = new Mock<IRepository<Contributor>>();
+  private readonly Mock<IMediator> _mockMediator = new Mock<IMediator>();
+  private readonly DeleteContributorService _service;
 
-        public DeleteContributorService_DeleteContributor()
-        {
-            _service = new DeleteContributorService(_mockRepo.Object, _mockMediator.Object);
-        }
+  public DeleteContributorService_DeleteContributor()
+  {
+    _service = new DeleteContributorService(_mockRepo.Object, _mockMediator.Object);
+  }
 
-        [Fact]
-        public async Task ReturnsNotFoundGivenCantFindContributor()
-        {
-            var result = await _service.DeleteContributor(0);
+  [Fact]
+  public async Task ReturnsNotFoundGivenCantFindContributor()
+  {
+    var result = await _service.DeleteContributor(0);
 
-            Assert.Equal(Ardalis.Result.ResultStatus.NotFound, result.Status);
-        }
-    }
+    Assert.Equal(Ardalis.Result.ResultStatus.NotFound, result.Status);
+  }
 }
