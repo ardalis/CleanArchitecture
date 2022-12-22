97062f9727cbc9d72c9beda5671b1120a6b5d908
Joseph Wambura <joseph.wambura@pycs.co.ke>
2022-12-22 14:01:06 +0300

    update web app to the latest default template web app

--- src/Clean.Architecture.Web/Controllers/HomeController.cs
+++ src/Clean.Architecture.Web/Controllers/HomeController.cs
@@ -16,7 +16,4 @@ public class HomeController : Controller
   {
     return View();
   }
-
-  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-  public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 }
