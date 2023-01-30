e6449f5cd6659baf06e2bcf55b75b529c37866ba
TarasKovalenko <bad.kto@gmail.com>
2023-01-30 13:23:54 +0200

    feat: added cancellation token usage

--- src/Clean.Architecture.Core/ProjectAggregate/Handlers/ContributorDeletedHandler.cs
+++ src/Clean.Architecture.Core/ProjectAggregate/Handlers/ContributorDeletedHandler.cs
@@ -17,14 +17,14 @@ public class ContributorDeletedHandler : INotificationHandler<ContributorDeleted
   public async Task Handle(ContributorDeletedEvent domainEvent, CancellationToken cancellationToken)
   {
     var projectSpec = new ProjectsWithItemsByContributorIdSpec(domainEvent.ContributorId);
-    var projects = await _repository.ListAsync(projectSpec);
+    var projects = await _repository.ListAsync(projectSpec, cancellationToken);
     foreach (var project in projects)
     {
       project.Items
         .Where(item => item.ContributorId == domainEvent.ContributorId)
         .ToList()
         .ForEach(item => item.RemoveContributor());
-      await _repository.UpdateAsync(project);
+      await _repository.UpdateAsync(project, cancellationToken);
     }
   }
 }
--- src/Clean.Architecture.Web/Endpoints/ContributorEndpoints/List.cs
+++ src/Clean.Architecture.Web/Endpoints/ContributorEndpoints/List.cs
@@ -30,6 +30,6 @@ public class List : EndpointWithoutRequest<ContributorListResponse>
         .ToList()
     };
 
-    await SendAsync(response);
+    await SendAsync(response, cancellation: cancellationToken);
   }
 }
