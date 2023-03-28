57a9fc72c0d445a047a08ff6790dc0dfcb931e98
Simone Guasconi <103770092+simoneguasconi@users.noreply.github.com>
2023-03-28 12:28:28 +0200

    Update StartupSetup.cs
    Add null check to connection string parameter

--- src/Clean.Architecture.Infrastructure/StartupSetup.cs
+++ src/Clean.Architecture.Infrastructure/StartupSetup.cs
@@ -6,7 +6,25 @@ namespace Clean.Architecture.Infrastructure;
 
 public static class StartupSetup
 {
+  /*
   public static void AddDbContext(this IServiceCollection services, string connectionString) =>
       services.AddDbContext<AppDbContext>(options =>
           options.UseSqlite(connectionString)); // will be created in web project root
+  */
+  
+  public static void AddDbContext(this IServiceCollection services, string? connectionString = null)
+  {
+    if (connectionString == null)
+    {
+      var builder = new SqliteConnectionStringBuilder()
+      {
+        DataSource = "Your database file",
+        DefaultTimeout = 10,
+      };
+
+      connectionString = builder.ConnectionString;
+    }
+
+    services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
+  }
 }
