6234126f1457e9e0db2d10edbb6ef468cc737c44
Micky McQuade <micky@mcquade.com>
2024-02-19 14:21:58 -0600

    Fix Contributor List SQL

--- src/Clean.Architecture.Infrastructure/Data/Queries/ListContributorsQueryService.cs
+++ src/Clean.Architecture.Infrastructure/Data/Queries/ListContributorsQueryService.cs
@@ -12,7 +12,7 @@ public class ListContributorsQueryService(AppDbContext _db) : IListContributorsQ
   {
     // NOTE: This will fail if testing with EF InMemory provider
     var result = await _db.Database.SqlQuery<ContributorDTO>(
-      $"SELECT Id, Name, PhoneNumber_Number FROM Contributors") // don't fetch other big columns
+      $"SELECT Id, Name, COALESCE(PhoneNumber_Number, '') AS PhoneNumber FROM Contributors") // don't fetch other big columns
       //.Select(c => new ContributorDTO(c.Id, c.Name, c.PhoneNumber?.Number ?? ""))
       .ToListAsync();
 
