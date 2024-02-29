dfc4b02718ac3f6adba337bb05bbb38f7ac831b6
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2024-02-28 21:50:25 -0500

    Update Contributor PhoneNumber nullability for consistency (#686)
    * Adjust PhoneNumber nullability throughout layers
    
    * remove wwwroot from csproj
    
    * add LINQ expression back in for in memory provider

--- src/Clean.Architecture.UseCases/Contributors/ContributorDTO.cs
+++ src/Clean.Architecture.UseCases/Contributors/ContributorDTO.cs
@@ -1,2 +1,2 @@
 ﻿namespace Clean.Architecture.UseCases.Contributors;
-public record ContributorDTO(int Id, string Name, string PhoneNumber);
+public record ContributorDTO(int Id, string Name, string? PhoneNumber);
