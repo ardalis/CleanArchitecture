71ccb8cae67d9aa2ee30c62ff5dbc16da3e24870
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2024-02-28 19:16:03 -0500

    add LINQ expression back in for in memory provider

--- src/Clean.Architecture.Infrastructure/Data/Queries/ListContributorsQueryService.cs
+++ src/Clean.Architecture.Infrastructure/Data/Queries/ListContributorsQueryService.cs
@@ -13,6 +13,7 @@ public class ListContributorsQueryService(AppDbContext _db) : IListContributorsQ
     // NOTE: This will fail if testing with EF InMemory provider
     var result = await _db.Database.SqlQuery<ContributorDTO>(
       $"SELECT Id, Name, PhoneNumber_Number AS PhoneNumber FROM Contributors") // don't fetch other big columns
+      //.Select(c => new ContributorDTO(c.Id, c.Name, c.PhoneNumber?.Number))
       .ToListAsync();
 
     return result;
