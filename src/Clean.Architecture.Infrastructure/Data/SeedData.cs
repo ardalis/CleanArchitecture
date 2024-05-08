9ff2ee5d451a4a1b3350699481a09aeea692e302
Saman Azadi <60857846+samanazadi1996@users.noreply.github.com>
2024-05-08 14:38:42 +0330

    Adding async capability to database seeding operations (#734)

--- src/Clean.Architecture.Infrastructure/Data/SeedData.cs
+++ src/Clean.Architecture.Infrastructure/Data/SeedData.cs
@@ -1,35 +1,22 @@
 ﻿using Clean.Architecture.Core.ContributorAggregate;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Clean.Architecture.Infrastructure.Data;
 
 public static class SeedData
 {
-  public static readonly Contributor Contributor1 = new("Ardalis");
-  public static readonly Contributor Contributor2 = new("Snowfrog");
-
-  public static void Initialize(IServiceProvider serviceProvider)
+  public static async Task InitializeAsync(AppDbContext dbContext)
   {
-    using (var dbContext = new AppDbContext(
-        serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>(), null))
-    {
-      if (dbContext.Contributors.Any()) return;   // DB has been seeded
+    if (await dbContext.Contributors.AnyAsync()) return; // DB has been seeded
 
-      PopulateTestData(dbContext);
-    }
+    await PopulateTestDataAsync(dbContext);
   }
-  public static void PopulateTestData(AppDbContext dbContext)
+
+  private static async Task PopulateTestDataAsync(AppDbContext dbContext)
   {
-    foreach (var contributor in dbContext.Contributors)
-    {
-      dbContext.Remove(contributor);
-    }
-    dbContext.SaveChanges();
+    Contributor[] Contributors = [new("Ardalis"), new("Snowfrog")];
 
-    dbContext.Contributors.Add(Contributor1);
-    dbContext.Contributors.Add(Contributor2);
-
-    dbContext.SaveChanges();
+    dbContext.Contributors.AddRange(Contributors);
+    await dbContext.SaveChangesAsync();
   }
 }
