45257c25359087bc94502e0346d8fc962d51ed10
AminSharifi <m.amin.sharifi@gmail.com>
2024-07-07 12:53:17 +0330

    KISS principle & fluent builder pattern

--- sample/src/NimblePros.SampleToDo.Core/ContributorAggregate/Contributor.cs
+++ sample/src/NimblePros.SampleToDo.Core/ContributorAggregate/Contributor.cs
@@ -5,15 +5,21 @@ namespace NimblePros.SampleToDo.Core.ContributorAggregate;
 
 public class Contributor : EntityBase, IAggregateRoot
 {
-  public string Name { get; private set; }
+  public string Name { get; private set; } = default!;
 
   public Contributor(string name)
   {
-    Name = Guard.Against.NullOrEmpty(name, nameof(name));
+    SetName(name);
   }
 
   public void UpdateName(string newName)
   {
-    Name = Guard.Against.NullOrEmpty(newName, nameof(newName));
+    SetName(newName);
+  }
+
+  public Contributor SetName(string newName)
+  {
+    this.Name = Guard.Against.NullOrEmpty(newName, nameof(newName));
+    return this;
   }
 }
