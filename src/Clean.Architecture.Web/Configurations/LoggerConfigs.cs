a72f36e80926b01c2a12806ec719c7c842240efe
Tadeh Boghosian <tadeh.boghosian@outlook.com>
2024-09-16 15:40:15 -0700

    Clean.Architecture.Web/Program.cs configuration over all clean up

---
+++ src/Clean.Architecture.Web/Configurations/LoggerConfigs.cs
@@ -0,0 +1,14 @@
+﻿using Serilog;
+
+namespace Clean.Architecture.Web.Configurations;
+
+public static class LoggerConfigs
+{
+  public static WebApplicationBuilder AddLoggerConfigs(this WebApplicationBuilder builder)
+  {
+
+    builder.Host.UseSerilog((_, config) => config.ReadFrom.Configuration(builder.Configuration));
+
+    return builder;
+  }
+}
--- src/Clean.Architecture.Web/Program.cs
+++ src/Clean.Architecture.Web/Program.cs
@@ -1,17 +1,11 @@
-﻿using System.Reflection;
-using Ardalis.ListStartupServices;
-using Ardalis.SharedKernel;
-using Clean.Architecture.Core.ContributorAggregate;
-using Clean.Architecture.Core.Interfaces;
-using Clean.Architecture.Infrastructure;
-using Clean.Architecture.Infrastructure.Data;
-using Clean.Architecture.Infrastructure.Email;
-using Clean.Architecture.UseCases.Contributors.Create;
+﻿using Clean.Architecture.Web.Configurations;
 using FastEndpoints;
 using FastEndpoints.Swagger;
-using MediatR;
 using Serilog;
 using Serilog.Extensions.Logging;
+
+
+var builder = WebApplication.CreateBuilder(args);
 
 var logger = Log.Logger = new LoggerConfiguration()
   .Enrich.FromLogContext()
@@ -20,18 +14,13 @@ var logger = Log.Logger = new LoggerConfiguration()
 
 logger.Information("Starting web host");
 
-var builder = WebApplication.CreateBuilder(args);
+builder.AddLoggerConfigs();
 
-builder.Host.UseSerilog((_, config) => config.ReadFrom.Configuration(builder.Configuration));
 var microsoftLogger = new SerilogLoggerFactory(logger)
     .CreateLogger<Program>();
 
-// Configure Web Behavior
-builder.Services.Configure<CookiePolicyOptions>(options =>
-{
-  options.CheckConsentNeeded = context => true;
-  options.MinimumSameSitePolicy = SameSiteMode.None;
-});
+builder.Services.AddOptionConfigs(builder.Configuration, microsoftLogger, builder);
+builder.Services.AddServiceConfigs(microsoftLogger, builder);
 
 builder.Services.AddFastEndpoints()
                 .SwaggerDocument(o =>
@@ -39,91 +28,13 @@ builder.Services.AddFastEndpoints()
                   o.ShortSchemaNames = true;
                 });
 
-ConfigureMediatR();
-
-builder.Services.AddInfrastructureServices(builder.Configuration, microsoftLogger);
-
-if (builder.Environment.IsDevelopment())
-{
-  // Use a local test email server
-  // See: https://ardalis.com/configuring-a-local-test-email-server/
-  builder.Services.AddScoped<IEmailSender, MimeKitEmailSender>();
-
-  // Otherwise use this:
-  //builder.Services.AddScoped<IEmailSender, FakeEmailSender>();
-  AddShowAllServicesSupport();
-}
-else
-{
-  builder.Services.AddScoped<IEmailSender, MimeKitEmailSender>();
-}
-
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-  app.UseDeveloperExceptionPage();
-  app.UseShowAllServicesMiddleware(); // see https://github.com/ardalis/AspNetCoreStartupServices
-}
-else
-{
-  app.UseDefaultExceptionHandler(); // from FastEndpoints
-  app.UseHsts();
-}
+await app.UseWebApplicationConfigs();
 
-app.UseFastEndpoints()
-    .UseSwaggerGen(); // Includes AddFileServer and static files middleware
-
-app.UseHttpsRedirection();
-
-await SeedDatabase(app);
 
 app.Run();
 
-static async Task SeedDatabase(WebApplication app)
-{
-  using var scope = app.Services.CreateScope();
-  var services = scope.ServiceProvider;
-
-  try
-  {
-    var context = services.GetRequiredService<AppDbContext>();
-    //          context.Database.Migrate();
-    context.Database.EnsureCreated();
-    await SeedData.InitializeAsync(context);
-  }
-  catch (Exception ex)
-  {
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An error occurred seeding the DB. {exceptionMessage}", ex.Message);
-  }
-}
-
-void ConfigureMediatR()
-{
-  var mediatRAssemblies = new[]
-{
-  Assembly.GetAssembly(typeof(Contributor)), // Core
-  Assembly.GetAssembly(typeof(CreateContributorCommand)) // UseCases
-};
-  builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(mediatRAssemblies!));
-  builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
-  builder.Services.AddScoped<IDomainEventDispatcher, MediatRDomainEventDispatcher>();
-}
-
-void AddShowAllServicesSupport()
-{
-  // add list services for diagnostic purposes - see https://github.com/ardalis/AspNetCoreStartupServices
-  builder.Services.Configure<ServiceConfig>(config =>
-  {
-    config.Services = new List<ServiceDescriptor>(builder.Services);
-
-    // optional - default path to view services is /listallservices - recommended to choose your own path
-    config.Path = "/listservices";
-  });
-}
 
 // Make the implicit Program.cs class public, so integration tests can reference the correct assembly for host building
-public partial class Program
-{
-}
+public partial class Program { }
