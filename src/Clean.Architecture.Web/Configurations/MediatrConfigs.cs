84ab0e45363d18dbf93bc40d01c1ad5006a8f67e
Tadeh Boghosian <99886944+TadehB7@users.noreply.github.com>
2024-09-26 12:56:25 -0700

    Clean.Architecture.Web/Program.cs configuration over all clean up (#821)
    Co-authored-by: Tadeh Boghosian <tadeh.boghosian@outlook.com>
    Co-authored-by: Steve Smith <steve@kentsmiths.com>

---
+++ src/Clean.Architecture.Web/Configurations/MediatrConfigs.cs
@@ -0,0 +1,25 @@
+﻿using Ardalis.SharedKernel;
+using Clean.Architecture.Core.ContributorAggregate;
+using Clean.Architecture.UseCases.Contributors.Create;
+using MediatR;
+using System.Reflection;
+
+namespace Clean.Architecture.Web.Configurations;
+
+public static class MediatrConfigs
+{
+  public static IServiceCollection AddMediatrConfigs(this IServiceCollection services)
+  {
+    var mediatRAssemblies = new[]
+      {
+        Assembly.GetAssembly(typeof(Contributor)), // Core
+        Assembly.GetAssembly(typeof(CreateContributorCommand)) // UseCases
+      };
+
+    services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(mediatRAssemblies!))
+            .AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>))
+            .AddScoped<IDomainEventDispatcher, MediatRDomainEventDispatcher>();
+
+    return services;
+  }
+}
---
+++ src/Clean.Architecture.Web/Configurations/WebApplicatonConfigs.cs
@@ -0,0 +1,53 @@
+﻿using Ardalis.ListStartupServices;
+using Clean.Architecture.Infrastructure.Data;
+using FastEndpoints;
+using FastEndpoints.Swagger;
+
+namespace Clean.Architecture.Web.Configurations;
+
+public static class WebApplicatonConfigs
+{
+  public static async Task<IApplicationBuilder> UseWebApplicationConfigs(this WebApplication app)
+  {
+    if (app.Environment.IsDevelopment())
+    {
+      app.UseDeveloperExceptionPage();
+      app.UseShowAllServicesMiddleware(); // see https://github.com/ardalis/AspNetCoreStartupServices
+    }
+    else
+    {
+      app.UseDefaultExceptionHandler(); // from FastEndpoints
+      app.UseHsts();
+    }
+
+    app.UseFastEndpoints()
+        .UseSwaggerGen(); // Includes AddFileServer and static files middleware
+
+
+    app.UseHttpsRedirection();
+
+    await SeedDatabase(app);
+
+    return app;
+
+  }
+
+  static async Task SeedDatabase(WebApplication app)
+  {
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+
+    try
+    {
+      var context = services.GetRequiredService<AppDbContext>();
+      //          context.Database.Migrate();
+      context.Database.EnsureCreated();
+      await SeedData.InitializeAsync(context);
+    }
+    catch (Exception ex)
+    {
+      var logger = services.GetRequiredService<ILogger<Program>>();
+      logger.LogError(ex, "An error occurred seeding the DB. {exceptionMessage}", ex.Message);
+    }
+  }
+}
