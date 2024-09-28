38eb041a4372ff30ca68e0e540daae5240bfb294
Steve Smith <steve@kentsmiths.com>
2024-09-28 11:22:04 -0400

    Fixes #746

--- sample/src/NimblePros.SampleToDo.Web/SeedData.cs
+++ sample/src/NimblePros.SampleToDo.Web/SeedData.cs
@@ -9,7 +9,7 @@ public static class SeedData
 {
   public static readonly Contributor Contributor1 = new ("Ardalis");
   public static readonly Contributor Contributor2 = new ("Snowfrog");
-  public static readonly Project TestProject1 = new Project("Test Project", PriorityStatus.Backlog);
+  public static readonly Project TestProject1 = new Project("Test Project", Priority.Backlog);
   public static readonly ToDoItem ToDoItem1 = new ToDoItem
   {
     Title = "Get Sample Working",
