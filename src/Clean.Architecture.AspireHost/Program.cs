26d737dab421f8c3a26916eaebdf0f2873f00072
Benjamin Moloise Mishra <benjaminmishra@gmail.com>
2024-10-30 00:34:28 +0100

    Final commit for adding aspire

--- src/Clean.Architecture.AspireHost/Program.cs
+++ src/Clean.Architecture.AspireHost/Program.cs
@@ -1,7 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder
-.AddProject<Projects.Clean_Architecture_Web>("web")
-.WithExternalHttpEndpoints();
+builder.AddProject<Projects.Clean_Architecture_Web>("web");
 
 builder.Build().Run();
--- tests/Clean.Architecture.AspireTests/AspireIntegrationTests.cs
+++ tests/Clean.Architecture.AspireTests/AspireIntegrationTests.cs
@@ -2,27 +2,6 @@ namespace Clean.Architecture.AspireTests.Tests;
 
 public class AspireIntegrationTests
 {
-    [Fact]
-    public async Task GetContributorsReturnsOkStatusCode()
-    {
-        // Arrange
-        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.Clean_Architecture_AspireHost>();
-        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
-        {
-            clientBuilder.AddStandardResilienceHandler();
-        });
-        // To output logs to the xUnit.net ITestOutputHelper, consider adding a package from https://www.nuget.org/packages?q=xunit+logging
-    
-        await using var app = await appHost.BuildAsync();
-        var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
-        await app.StartAsync();
-
-        // Act
-        var httpClient = app.CreateHttpClient("web");
-        await resourceNotificationService.WaitForResourceAsync("web", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
-        var response = await httpClient.GetAsync("/Contributors");
-
-        // Assert
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-    }
+    // Follow the link below to write you tests with Aspire
+    // https://learn.microsoft.com/en-us/dotnet/aspire/testing/write-your-first-test?pivots=xunit
 }
