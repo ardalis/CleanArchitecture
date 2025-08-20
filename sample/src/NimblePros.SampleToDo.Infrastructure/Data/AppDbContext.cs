518daba253e9bf86ddc77cdafb7e93e6b9924fae
Samson Nwizugbe <samson6398@gmail.com>
2025-08-20 20:11:05 +0100

    Domain Event Dispatcher In Save Changes Pipeline Via Interceptor (#957)
    * Invert Depedency of AppDbContext on Event Dispatcher By Creating an EF Core Interceptor That retrieves the entities and dispatches events after save changes has completed successfully
    
    * Delete NoOpDomainEventDispatcher Introduced in Infrastructure service extension.
    Domain Event is configured in the web project using the Ardalis sharedKernel package
    
    ---------
    
    Co-authored-by: Steve Smith <steve@kentsmiths.com>

--- sample/src/NimblePros.SampleToDo.Infrastructure/Data/AppDbContext.cs
+++ sample/src/NimblePros.SampleToDo.Infrastructure/Data/AppDbContext.cs
@@ -5,14 +5,9 @@ namespace NimblePros.SampleToDo.Infrastructure.Data;
 
 public class AppDbContext : DbContext
 {
-  private readonly IDomainEventDispatcher? _dispatcher;
 
-  public AppDbContext(DbContextOptions<AppDbContext> options,
-    IDomainEventDispatcher? dispatcher)
-      : base(options)
-  {
-    _dispatcher = dispatcher;
-  }
+  public AppDbContext(DbContextOptions<AppDbContext> options)
+      : base(options) {}
 
   public DbSet<ToDoItem> ToDoItems => Set<ToDoItem>();
   public DbSet<Project> Projects => Set<Project>();
@@ -29,24 +24,6 @@ public class AppDbContext : DbContext
     base.ConfigureConventions(configurationBuilder);
 
     //configurationBuilder.RegisterAllInVogenEfCoreConverters();
-  }
-
-  public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
-  {
-    int result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-
-    // ignore events if no dispatcher provided
-    if (_dispatcher == null) return result;
-
-    // dispatch events only if save was successful
-    var entitiesWithEvents = ChangeTracker.Entries<HasDomainEventsBase>()
-        .Select(e => e.Entity)
-        .Where(e => e.DomainEvents.Any())
-        .ToArray();
-
-    await _dispatcher.DispatchAndClearEvents(entitiesWithEvents);
-
-    return result;
   }
 
   public override int SaveChanges()
--- src/Clean.Architecture.Infrastructure/Data/AppDbContext.cs
+++ src/Clean.Architecture.Infrastructure/Data/AppDbContext.cs
@@ -1,35 +1,14 @@
 ﻿using Clean.Architecture.Core.ContributorAggregate;
 
 namespace Clean.Architecture.Infrastructure.Data;
-public class AppDbContext(DbContextOptions<AppDbContext> options,
-  IDomainEventDispatcher? dispatcher) : DbContext(options)
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-  private readonly IDomainEventDispatcher? _dispatcher = dispatcher;
-
   public DbSet<Contributor> Contributors => Set<Contributor>();
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
     base.OnModelCreating(modelBuilder);
     modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-  }
-
-  public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
-  {
-    int result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-
-    // ignore events if no dispatcher provided
-    if (_dispatcher == null) return result;
-
-    // dispatch events only if save was successful
-    var entitiesWithEvents = ChangeTracker.Entries<HasDomainEventsBase>()
-        .Select(e => e.Entity)
-        .Where(e => e.DomainEvents.Any())
-        .ToArray();
-
-    await _dispatcher.DispatchAndClearEvents(entitiesWithEvents);
-
-    return result;
   }
 
   public override int SaveChanges() =>
