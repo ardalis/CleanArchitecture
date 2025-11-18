3b1b99d04992527487fad620f94fa910cad49da3
Steve Smith <steve@kentsmiths.com>
2025-11-18 11:28:04 -0500

    MinimalTemplate Cleanup (#979)
    * Implement endpoint; clean up legacy artifacts
    
    * scrub mediatr from repo
    
    * update nuspec

--- MinimalClean/src/MinimalClean.Architecture.Web/Domain/ProductAggregate/Product.cs
+++ MinimalClean/src/MinimalClean.Architecture.Web/Domain/ProductAggregate/Product.cs
@@ -1,15 +1,28 @@
+using Ardalis.GuardClauses;
+
 namespace MinimalClean.Architecture.Web.Domain.ProductAggregate;
 
 public class Product : EntityBase<Product, ProductId>, IAggregateRoot
 {
+  // Private constructor for EF Core
+  private Product() { }
+
   public Product(ProductId id, string name, decimal unitPrice)
   {
+    Guard.Against.Equal(id, ProductId.New, nameof(id), 
+      "Use Product.Create() to create new products instead of passing ProductId.New to the constructor.");
     Id = id;
     Name = name;
     UnitPrice = unitPrice;
   }
 
-  public string Name { get; private set; }
+  // Factory method for creating new products (before persistence)
+  public static Product Create(string name, decimal unitPrice)
+  {
+    return new Product(ProductId.New, name, unitPrice);
+  }
+
+  public string Name { get; private set; } = string.Empty;
   public decimal UnitPrice { get; private set; }
 
   public Product UpdateName(string newName)
